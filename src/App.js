0e77c9822ab54dfbad80e18dc7ab2bfbd796c9f6
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-07-09 12:02:32 +0100

    Adding a movie and connecting to Firebase -initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 
 import MoviesList from './components/MoviesList'
+import AddMovie from './components/AddMovie'
 import './App.css'
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
     setIsLoading(true)
     setError(null)
     try {
-      const response = await fetch('https://swapi.dev/api/films')
+      const response = await fetch(
+        'https://academind-react-databases-app-default-rtdb.europe-west1.firebasedatabase.app/movies.json '
+      )
       if (!response.ok) {
         throw new Error('Something went wrong:S')
       }
@@ -38,6 +41,9 @@ function App() {
     fetchMoviesHandler()
   }, [fetchMoviesHandler])
 
+  function addMovieHandler(movie) {
+    console.log(movie)
+  }
   let content = <p>Found no movies.</p>
 
   if (movies.length > 0) {
@@ -54,6 +60,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
